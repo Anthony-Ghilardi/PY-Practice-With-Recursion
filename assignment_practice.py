871ead628f605be36c0a7a64b1a1e1a36912811e
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-08-19 15:43:46 -0700

    Completed

--- assignment_practice.py
+++ assignment_practice.py
@@ -35,7 +35,12 @@
 
 
 # FUNCTION 3
+# def rev_string(x):
+#     return x[::-1]
 
+# reversedString = rev_string("Wow who knew python was so cool")
+
+# print(reversedString)
 
 
 
@@ -44,7 +49,17 @@
 
 
 # FUNCTION 4
+# def num_within(num, start, end):
+#     if num >= start and num <= end:
+#         return True
+#     else:
+#         return False
+    
+# num = 2 # Change this number to test the function
+# start = 1
+# end = 10
 
+# print(num_within(num, start, end))
 
 
 
@@ -53,3 +68,30 @@
 
 
 # FUNCTION 5 
+# triangle = [[1],[1,1]]
+# def pascal(n):
+#   if n < 1:
+#     print("invalid number of rows")
+#   elif n == 1:
+#     print(triangle[0])
+#   else:
+#     row_number = 2
+#     while len(triangle) < n:
+#       row = []
+#       row_prev = triangle[row_number - 1]
+#       length = len(row_prev)+1
+#       for i in range(length):
+#         if i == 0:
+#           row.append(1)
+#         elif i > 0 and i < length-1:
+#           row.append(triangle[row_number-1][i-1]+triangle[row_number-1][i])
+#         else:
+#           row.append(1)
+#       triangle.append(row)
+#       row_number += 1
+
+#     for row in triangle:
+#       print(row)
+
+# pascal(2)
+# pascal(5)
